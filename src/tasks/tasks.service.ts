d15ac52bd8f5b35e36aba275bd1d147f02ebe813
libinthomas33 <30334126+libinthomas33@users.noreply.github.com>
2024-02-16 16:45:38 +0530

    Add validation for API request (#9)

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -13,7 +13,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID "${id}" not found`);
+    }
+
+    return task;
   }
 
   getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
@@ -51,7 +57,8 @@ export class TasksService {
   }
 
   deleteTaskById(id: string): void {
-    const index = this.tasks.findIndex((task) => task.id === id);
+    const task = this.getTaskById(id);
+    const index = this.tasks.findIndex((t) => t.id === task.id);
     if (index != -1) {
       this.tasks.splice(index, 1);
     }
