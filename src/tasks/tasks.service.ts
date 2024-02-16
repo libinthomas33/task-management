932d37b645f418cac6943a05b720bff5455f6fa2
libinthomas33 <30334126+libinthomas33@users.noreply.github.com>
2024-02-16 12:58:34 +0530

    Add task delete API (#7)

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -27,4 +27,11 @@ export class TasksService {
 
     return task;
   }
+
+  DeleteTaskById(id: string): void {
+    const index = this.tasks.findIndex((task) => task.id === id);
+    if (index != -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
 }
