3c6ca70113a307e40c46f98b76dbc789637af372
libinthomas33 <libinthomas33@gmail.com>
2024-02-19 13:42:43 +0530

    Fix - test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,38 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import supertest from 'supertest';
 import { AppModule } from './../src/app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import supertest from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
-  let connection: Connection;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [
-        AppModule,
-        // Mock TypeORM module
-        TypeOrmModule.forRoot({
-          type: 'sqlite', // Use an in-memory SQLite database for testing
-          database: ':memory:',
-          entities: [], // Provide empty array to prevent loading entities
-          synchronize: true,
-        }),
-      ],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
-
-    // Get the TypeORM connection and repository
-    connection = moduleFixture.get<Connection>(Connection);
   });
 
   afterEach(async () => {
-    // Close the TypeORM connection after each test
-    await connection.close();
+    if (app) {
+      await app.close();
+    }
   });
 
   it('/ (GET)', () => {
