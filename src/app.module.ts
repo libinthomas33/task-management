f14ea8b4194f6b3a87cba2b63a6aee60609cbb34
libinthomas33 <30334126+libinthomas33@users.noreply.github.com>
2024-02-19 14:09:16 +0530

    Add .env file and updated test cases (#12)
    * Add .env file and updated test cases

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+
 @Module({
   imports: [
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER_NAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
     }),
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import supertest from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const moduleFixture: TestingModule = await Test.createTestingModule(
+      {},
+    ).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
+  afterEach(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(404);
+    return supertest(app.getHttpServer()).get('/').expect(404);
   });
 });
