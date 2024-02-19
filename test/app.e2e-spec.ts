cf740d3b55c6e0b89a1c1c8f1948e6de7e6f4784
libinthomas33 <libinthomas33@gmail.com>
2024-02-19 12:02:04 +0530

    Add .env file and updated test cases

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,21 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import supertest from 'supertest';
 import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let connection: Connection;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        // Mock TypeORM module
+        TypeOrmModule.forRoot({
+          type: 'sqlite', // Use an in-memory SQLite database for testing
+          database: ':memory:',
+          entities: [], // Provide empty array to prevent loading entities
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    // Get the TypeORM connection and repository
+    connection = moduleFixture.get<Connection>(Connection);
+  });
+
+  afterEach(async () => {
+    // Close the TypeORM connection after each test
+    await connection.close();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(404);
+    return supertest(app.getHttpServer()).get('/').expect(404);
   });
 });
